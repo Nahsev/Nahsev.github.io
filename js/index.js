7c33090d25f251c3d8128ae274fcd88afe24c4c7
Tomas Blazquez <tomasnahsev@hotmail.com>
2023-01-31 18:36:49 -0300

    Proyecto final

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.getElementById('fa-map-marker').addEventListener('click', function(){
+    document.getElementById("textocambia").innerHTML = 'Enoree, South Carolina, EEUU'
+})
+
+document.getElementById('fa-birthday-cake').addEventListener('click', function(){
+    document.getElementById("textocambia").innerHTML = '11/1/1963'
+})
+
+document.getElementById('fa-envelope-o').addEventListener('click', function(){
+    document.getElementById("textocambia").innerHTML = 'connor.howard@example.com'
+})
+
+document.getElementById('fa-phone').addEventListener('click', function(){
+    document.getElementById("textocambia").innerHTML = '(771) 969-6892'
+})
